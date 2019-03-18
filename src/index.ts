fbe841f55e74296ae4358f93b97312fc5b5fc024
el-davo <david.ahern@emc.com>
2019-03-18 11:21:59 +0000

    Fixing code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig } from '@loopback/core';
 import { GraphqlApplication } from './application';
 
-const {PORT, USER_API, PROJECTS_API, JOBS_API, TEST_IMAGE} = process.env;
+const {PORT, USER_API, PROJECTS_API, JOBS_API, AMQP_URL, AMQP_PORT, AMQP_USER, AMQP_PWD, TEST_IMAGE} = process.env;
 
 export async function main(options: ApplicationConfig = {}): Promise<GraphqlApplication> {
     options.port = options.port || PORT;
@@ -9,6 +9,12 @@ export async function main(options: ApplicationConfig = {}): Promise<GraphqlAppl
         userApi: USER_API,
         projectsApi: PROJECTS_API,
         jobsApi: JOBS_API
+    };
+    options.amqp = {
+        url: AMQP_URL,
+        port: (options.amqp || {}).port || AMQP_PORT,
+        user: AMQP_USER,
+        pwd: AMQP_PWD
     };
     options.kubernetes = {
         testImage: TEST_IMAGE
--- src/services/Kubernetes.service.ts
+++ src/services/Kubernetes.service.ts
@@ -15,45 +15,73 @@ export class KubernetesService {
     @inject('kubernetes.client')
     private kubernetesClient: ApiRoot;
 
+    @inject('amqp.url')
+    amqpUrl: string;
+
+    @inject('amqp.port')
+    amqpPort: string;
+
+    @inject('amqp.user')
+    amqpUser: string;
+
+    @inject('amqp.pwd')
+    amqpPwd: string;
+
     async startTestPod(job: Job, task: Task) {
         const id = uuid();
 
         await this.kubernetesClient.api.v1.namespaces('default').pod.post({
             body: {
-                "kind": "Pod",
-                "apiVersion": "v1",
-                "metadata": {
-                    "labels": {
-                        "app": "ws-flare-test-client"
+                kind: "Pod",
+                apiVersion: "v1",
+                metadata: {
+                    labels: {
+                        app: "ws-flare-test-client"
                     },
-                    "name": `ws-flare-test-client-${id}`
+                    name: `ws-flare-test-client-${id}`
                 },
-                "spec": {
-                    "containers": [
+                spec: {
+                    containers: [
                         {
-                            "name": `ws-flare-test-client-${id}`,
-                            "image": this.testImage,
-                            "ports": [
+                            name: `ws-flare-test-client-${id}`,
+                            image: this.testImage,
+                            ports: [
                                 {
-                                    "containerPort": 80
+                                    containerPort: 80
                                 }
                             ],
-                            "env": [
+                            env: [
                                 {
-                                    "name": "TASK_ID",
-                                    "value": task.id
+                                    name: "TASK_ID",
+                                    value: task.id
                                 },
                                 {
-                                    "name": "JOB_ID",
-                                    "value": job.id
+                                    name: "JOB_ID",
+                                    value: job.id
                                 },
                                 {
-                                    "name": "JOBS_API",
-                                    "value": this.jobsApi
+                                    name: "JOBS_API",
+                                    value: this.jobsApi
                                 },
                                 {
-                                    "name": "TOTAL_SIMULATED_USERS",
-                                    "value": task.totalSimulatedUsers
+                                    name: "TOTAL_SIMULATED_USERS",
+                                    value: task.totalSimulatedUsers
+                                },
+                                {
+                                    name: "AMQP_URL",
+                                    value: this.amqpUrl
+                                },
+                                {
+                                    name: "AMQP_PORT",
+                                    value: this.amqpPort
+                                },
+                                {
+                                    name: "AMQP_USER",
+                                    value: this.amqpUser
+                                },
+                                {
+                                    name: "AMQP_PWD",
+                                    value: this.amqpPwd
                                 }
                             ]
                         }
